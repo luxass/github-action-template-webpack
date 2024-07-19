d8e8e1bf5db50fc5bbee96efdce1648b6d25fad8
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-07-19 07:09:48 +0200

    chore: initial commit

---
+++ webpack.config.mjs
@@ -0,0 +1,35 @@
+// @ts-check
+import path from "node:path";
+
+export default {
+  target: "node",
+  mode: "production",
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(import.meta.dirname, "dist"),
+    filename: "index.js",
+    library: {
+      type: "commonjs2",
+    },
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: false,
+  },
+};
